a38ea286e01083da7ca79c7b115c4b4aceb85617
lute-gyabo-yasai <gyaboyan@gmail.com>
2020-09-26 21:46:30 +0900

    Update

--- xinput.cpp
+++ xinput.cpp
@@ -1,7 +1,8 @@
 //
 // XInputSandbox 2020 @gyabo
 //
-// ref:https://docs.microsoft.com/en-us/windows/win32/xinput/getting-started-with-xinput
+// ref:https://docs.microsoft.com/en-us/windows/win32/xinput/
+//
 #include <stdio.h>
 #include <windows.h>
 #include <stdlib.h>
@@ -12,30 +13,30 @@
 #include <algorithm>
 #include <xinput.h>
 
-#define INTERVAL_VIBRATION_CTRL 4
-//#define _DEBUG_GAMEPAD_
+#define INTERVAL_VIBRATION_CTRL	4
 
-void xinput_ctrl_vibration(int index, int left = 0, int right = 0)
+void
+xinput_ctrl_vibration(int index, int left = 0, int right = 0)
 {
 	XINPUT_VIBRATION vibration = {};
+
 	vibration.wLeftMotorSpeed = (std::max)(0, (std::min)(left, 0xFFFF));
 	vibration.wRightMotorSpeed = (std::max)(0, (std::min)(right, 0xFFFF));
 	XInputSetState(index, &vibration );
 }
 
-void xinput_debug(int index, XINPUT_STATE & state)
+void
+xinput_debug(int index, XINPUT_STATE & state)
 {
+	static int count = 0;
 	auto & gpad = state.Gamepad;
 
-
 	//Get input info.
-	printf("dev-%d:pktnum:%d, btn:0x%04X, LT:%d, RT:%d, stick:%d %d, %d %d\r",
-		index,
-		state.dwPacketNumber, gpad.wButtons,
+	printf("dev-%d:pkt:%d, btn:0x%04X, LT:%d, RT:%d, stick:%d %d, %d %d\r",
+		index, state.dwPacketNumber, gpad.wButtons,
 		gpad.bLeftTrigger, gpad.bRightTrigger,
 		gpad.sThumbLX, gpad.sThumbLY, gpad.sThumbRX, gpad.sThumbRY);
 
-	static int count = 0;
 	count++;
 	if ( (count % INTERVAL_VIBRATION_CTRL) == 0) {
 		auto LT = gpad.bLeftTrigger;
@@ -45,7 +46,8 @@ void xinput_debug(int index, XINPUT_STATE & state)
 
 }
 
-void xinput_ctrl()
+void
+xinput_ctrl()
 {
 	for (DWORD i = 0; i < XUSER_MAX_COUNT; i++ ) {
 		XINPUT_STATE state = {};
@@ -56,33 +58,52 @@ void xinput_ctrl()
 	}
 }
 
-void xinput_ctrl_get_battery_info() {
+void
+xinput_ctrl_get_battery_info()
+{
 	printf("\n\n============================\n");
 	for (DWORD i = 0; i < XUSER_MAX_COUNT; i++ ) {
 		printf("dev-%d:", i);
 
 		//Get Battery info.
 		XINPUT_BATTERY_INFORMATION batinfo = {};
-		XInputGetBatteryInformation(i, BATTERY_DEVTYPE_GAMEPAD, &batinfo);
+		XInputGetBatteryInformation(
+			i, BATTERY_DEVTYPE_GAMEPAD, &batinfo);
+		const char *type = "BATTERY_TYPE_DISCONNECTED";
+		const char *desc = "The device is not connected.";
+		printf("BatteryLevel=%d, ", batinfo.BatteryLevel);
 		if(batinfo.BatteryType == BATTERY_TYPE_DISCONNECTED) {
-			printf("BATTERY_TYPE_DISCONNECTED	The device is not connected.\n");
-			continue;
 		}
-		if(batinfo.BatteryType == BATTERY_TYPE_WIRED)
-			printf("BATTERY_TYPE_WIRED	The device is a wired device and does not have a battery.\n");
-		if(batinfo.BatteryType == BATTERY_TYPE_ALKALINE)
-			printf("BATTERY_TYPE_ALKALINE	The device has an alkaline battery.\n");
-		if(batinfo.BatteryType == BATTERY_TYPE_NIMH)
-			printf("BATTERY_TYPE_NIMH	The device has a nickel metal hydride battery.\n");
-		if(batinfo.BatteryType == BATTERY_TYPE_UNKNOWN)
-			printf("BATTERY_TYPE_UNKNOWN	The device has an unknown battery type.\n");
+		
+		if(batinfo.BatteryType == BATTERY_TYPE_WIRED) {
+			type = "BATTERY_TYPE_WIRED";
+			desc = "type wired device does not have a battery.";
+		}
+
+		if(batinfo.BatteryType == BATTERY_TYPE_ALKALINE) {
+			type = "BATTERY_TYPE_ALKALINE";
+			desc = "type alkaline battery";
+		}
+
+		if(batinfo.BatteryType == BATTERY_TYPE_NIMH) {
+			type = "BATTERY_TYPE_NIMH";
+			desc = "type nickel metal hydride battery.";
+		}
+
+		if(batinfo.BatteryType == BATTERY_TYPE_UNKNOWN) {
+			type = "BATTERY_TYPE_UNKNOWN";
+			desc = "The device has an unknown battery type.";
+		}
+		printf("%s\t: %s\n", type, desc);
 	}
 	printf("============================\n");
 }
 
-int main()
+int
+main()
 {
 	int count = 0;
+
 	while ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) == 0) {
 		xinput_ctrl();
 		count++;
